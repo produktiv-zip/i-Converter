a243dabf90e1b146af0fecb38953506ef457586a
Paul <produktiv.zip@web.de>
2023-05-29 13:45:20 +0200

    UI and File Update
    updated the ui and the file structure of the project

--- converter.py
+++
@@ -1,7 +0,0 @@
-def umrechnung_aepfel_in_bj(aepfel):
-    bj = aepfel * 0.3
-    return bj
-
-anzahl_aepfel = float(input("Gib die Anzahl der Äpfel ein: "))
-ergebnis_bj = umrechnung_aepfel_in_bj(anzahl_aepfel)
-print(f"{anzahl_aepfel} Äpfel entsprechen {ergebnis_bj} BJ.")
